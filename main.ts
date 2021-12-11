08ac09ce39f393b30147e1dcaabe2088b15d8fc9
Jürg Luthiger <juerg.luthiger@fhnw.ch>
2021-12-11 16:01:43 +0100

    test

--- main.ts
+++ main.ts
@@ -16,8 +16,8 @@ namespace drawrobot {
         driveCommand.setNumber(NumberFormat.UInt8LE, 0, 35);
         driveCommand.setNumber(NumberFormat.UInt8LE, 1, 77);
         driveCommand.setNumber(NumberFormat.UInt16LE, 2, vel);
-        // driveCommand.setNumber(NumberFormat.UInt32LE, 4, left*100);
-        // driveCommand.setNumber(NumberFormat.UInt32LE, 8, right*100);
+        driveCommand.setNumber(NumberFormat.UInt32LE, 4, left*100);
+        driveCommand.setNumber(NumberFormat.UInt32LE, 8, right*100);
         pins.i2cWriteBuffer(
             8,
             driveCommand,
